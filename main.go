27da76e3af9ef72e729b53a56d8fea28ecff0e3f
zzokki81 <zoran.rebic@outlook.com>
2020-02-06 11:24:31 +0100

    Added some pages

--- main.go
+++ main.go
@@ -1,17 +1,39 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from snippetbox!!"))
 }
 
+func showSnippet(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
+}
+
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method Not Allowed", 404)
+		return
+	}
+	w.Write([]byte("Create new snippet..."))
+
+}
+
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/show", showSnippet)
+	mux.HandleFunc("/create", createSnippet)
 	log.Println("Server started on :4000 ")
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
